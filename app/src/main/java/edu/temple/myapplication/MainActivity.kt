5cf1f63fbdd3a17e5ec4cba26ff8db2a626785dd
Thomas Ishida <ishida.thomas@hotmail.com>
2025-11-11 12:13:38 -0500

    working??? maybe????

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,21 +1,77 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
+import android.widget.TextView
+import java.util.logging.Handler
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var timerTextView: TextView
+    lateinit var timerBinder: TimerService.TimerBinder
+    var isConnected = false
+
+    var timerHandler = android.os.Handler(Looper.getMainLooper()) {
+        timerTextView.text = it.what.toString()
+        true
+    }
+
+    val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            timerBinder = service as TimerService.TimerBinder
+            timerBinder.setHandler(timerHandler)
+            isConnected = true
+        }
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isConnected = false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.startButton).setOnClickListener {
+        timerTextView = findViewById(R.id.textView)
 
+        bindService(
+            Intent(this, TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        )
+
+        with(findViewById<Button>(R.id.startButton)){
+            setOnClickListener {
+
+            if (isConnected) {
+                if(!timerBinder.isRunning) {
+                    timerBinder.start(100)
+                    text = "Pause"
+                    setBackgroundColor(0xFFFFA500.toInt())
+                }
+                else{
+                    timerBinder.pause()
+                    text = "Start"
+                    setBackgroundColor(0xFF00FF00.toInt())
+                }
+            }
         }
-        
-        findViewById<Button>(R.id.stopButton).setOnClickListener {
+        }
 
+        findViewById<Button>(R.id.stopButton).setOnClickListener {
+            if (isConnected) {
+                    timerBinder.stop()
+            }
         }
     }
+    override fun onDestroy() {
+        unbindService(serviceConnection)
+        super.onDestroy()
+    }
+
 }
